429faf9b755eaebf9f0c75f464ac43448dddad79
rexdev08 <raimundorincong@gmail.com>
2022-11-04 13:24:05 -0300

    modifique tamano y agregue link a mi portafolio

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <DrumMachine />
+      <a target={"_blank"} href='https://rexdev08.github.io/'>Hecho por Raimundo Rincon.</a>
     </div>
   );
 }
